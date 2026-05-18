10836ca7fe9e6e2017c5c95832b766256a72939c
meredithhector6-hash <meredithhector6@gmail.com>
2026-05-18 00:42:21 -0400

    Update Firebase configuration with new credentials

--- src/firebase.js
+++ src/firebase.js
@@ -10,12 +10,12 @@ import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey:            "REMPLACE_PAR_TA_CLE",
-  authDomain:        "REMPLACE.firebaseapp.com",
-  projectId:         "REMPLACE_PAR_TON_PROJECT_ID",
-  storageBucket:     "REMPLACE.appspot.com",
-  messagingSenderId: "REMPLACE",
-  appId:             "REMPLACE_PAR_TON_APP_ID"
+  apiKey:            "AIzaSyCvlbodvEAG0g8PwzFqVG_h0pVk61LZco0",
+  authDomain:        "aquimar-journal.firebaseapp.com",
+  projectId:         "aquimar-journal",
+  storageBucket:     "aquimar-journal.firebasestorage.app",
+  messagingSenderId: "396397169380",
+  appId:             "1:396397169380:web:c3953a614ebe78a3f77af1"
 };
 
 const app  = initializeApp(firebaseConfig);
